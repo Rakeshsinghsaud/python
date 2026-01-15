2a96089f338578526f85f947fae91aca9c182862
rakesh singh saud <rakesh23saud@gmail.com>
2026-01-15 21:48:03 +0545

    swap commit

--- swap.py
+++ swap.py
@@ -4,5 +4,5 @@ b = int(input('enter second number'))
 temp = a
 a =b
 b =  temp
-print('after swapping first number is')
-print ("after swapping second number is")
+print('after swapping first number is',a)
+print ("after swapping second number is",b)
