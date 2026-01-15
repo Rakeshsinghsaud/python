326448bb89d49f45f5ad754b835231a06974c077
rakesh singh saud <rakesh23saud@gmail.com>
2026-01-15 21:44:46 +0545

    swapping number commit

---
+++ swap.py
@@ -0,0 +1,8 @@
+#swap two user numbers
+a = int(input('enter first number'))
+b = int(input('enter second number'))
+temp = a
+a =b
+b =  temp
+print('after swapping first number is')
+print ("after swapping second number is")
