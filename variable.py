f3ed70ebc844e08ca93c6c0fe6b7f765b72b7a9e
rakesh singh saud <rakesh23saud@gmail.com>
2026-01-15 10:25:19 +0545

    varible in python  commit

---
+++ variable.py
@@ -0,0 +1,15 @@
+# Dynamic varible
+a=23
+print(a) #don't need to declare varible type
+
+# Static varible
+# int b=77 # deckare type of varible
+#print(b)
+
+#Dynamic binding
+a=23
+print(a) # single varible  can hold different types of data
+a="Rakesh"
+print(a)
+#Static binding
+int a=23 # varible can hold only one type of data
